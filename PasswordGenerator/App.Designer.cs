eb7eef49521d49baa246497b67dbb4f25d66de77
DevilishSasuke <petrovich_uasya@mail.ru>
2024-04-14 15:58:40 +0300

    added app logo

--- PasswordGenerator/App.Designer.cs
+++ PasswordGenerator/App.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
             SuspendLayout();
             // 
             // App
@@ -35,6 +36,7 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "App";
             Text = "Password Generator";
             Load += App_Load;
