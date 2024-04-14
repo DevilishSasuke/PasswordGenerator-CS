5c87359191776c38d67255b8eb8c53cfe0437fc0
DevilishSasuke <petrovich_uasya@mail.ru>
2024-04-14 15:53:13 +0300

    Добавьте файлы проекта.

---
+++ PasswordGenerator/App.cs
@@ -0,0 +1,15 @@
+namespace PasswordGenerator
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void App_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
